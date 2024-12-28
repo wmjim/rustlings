e110f4d7101bcebd727a0f72962c997beb3373e9
meng.wang <meng.w1016@outlook.com>
2024-12-28 16:42:07 +0800

    Finished 02_functions and 03_if

--- exercises/03_if/if1.rs
+++ exercises/03_if/if1.rs
@@ -4,10 +4,20 @@ fn bigger(a: i32, b: i32) -> i32 {
     // Do not use:
     // - another function call
     // - additional variables
+    if a > b {
+        a
+    } else if a == b {
+        a
+    } else {
+        b
+    }
 }
 
 fn main() {
     // You can optionally experiment here.
+    println!("The bigger number is: {}", bigger(10, 8));
+    println!("The bigger number is: {}", bigger(10, 11));
+    println!("The bigger number is: {}", bigger(10, 10));
 }
 
 // Don't mind this for now :)
