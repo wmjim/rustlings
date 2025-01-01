edf76232dcd83ca80b306f6d2150a23b0fda6980
meng.wang <meng.w1016@outlook.com>
2025-01-01 23:14:27 +0800

    finished 08_enums and 90_strings

--- exercises/10_modules/modules2.rs
+++ exercises/10_modules/modules2.rs
@@ -3,15 +3,15 @@
 
 mod delicious_snacks {
     // TODO: Add the following two `use` statements after fixing them.
-    // use self::fruits::PEAR as ???;
-    // use self::veggies::CUCUMBER as ???;
+    use self::fruits::PEAR as fruit;
+    use self::veggies::CUCUMBER as veggie;
 
-    mod fruits {
+    pub mod fruits {
         pub const PEAR: &str = "Pear";
         pub const APPLE: &str = "Apple";
     }
 
-    mod veggies {
+    pub mod veggies {
         pub const CUCUMBER: &str = "Cucumber";
         pub const CARROT: &str = "Carrot";
     }
